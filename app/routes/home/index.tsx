5fa2e629b55006e3919078e16c8e2d62785d4122
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-10-25 18:33:46 +0530

    Added Hero component

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/index";
+import Hero from "../../components/Hero";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <section> App</section>;
+  return (
+    <section>
+      <Hero />
+    </section>
+  );
 }
