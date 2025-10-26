c1e6be718a20331bb18d126b168ba00194c0f98f
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-10-26 13:52:15 +0530

    Fetcing projects with loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,22 @@
-const ProjectsPage = () => {
+import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+import axios from "axios";
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  try {
+    const res = await axios.get("http://localhost:8000/projects");
+    return { projects: res.data };
+  } catch (error) {
+    throw new Response("Failed to fetch projects", { status: 500 });
+  }
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as { projects: Project[] };
+  console.log(projects);
+
   return (
     <>
       <h2 className='text-3xl text-white font-bold mb-8'>My Projects</h2>
---
+++ app/types.ts
@@ -0,0 +1,9 @@
+export type Project = {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+  date: string;
+  category: boolean;
+};
